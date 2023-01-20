8b80b9a8f0e5230b9cadd8fbb61cbcfed56f8ae0
Joseph Kirkish <kirkish.2@gmail.com>
2023-01-20 14:01:27 -0500

    Update Assignment13Application.java

--- src/main/java/com/coderscampus/Assignment_13/Assignment13Application.java
+++ src/main/java/com/coderscampus/Assignment_13/Assignment13Application.java
@@ -11,13 +11,7 @@ public class Assignment13Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment13Application.class, args);
-		/*2 things left, when there is only one user, and i try to update the user, 
-		 * it deletes all the user information. and when i try to create an account, 
-		 * it redirects me to http://localhost:8080/users//accounts which gives me a 404 
-		 * not found error. again, when there are multiple users all is well
-		 * 
-		 * 
-		 */
+		
 	}
 
 }
